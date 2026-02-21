d1131e59744a691028700c956835f74808cd4b01
S1H2A3-art <jq2385@nyu.edu>
2026-02-21 17:08:58 -0500

    fix bug

--- Pages/Project_Pages/The_Secret_Life_of_a_Light_Bulb/project.js
+++ Pages/Project_Pages/The_Secret_Life_of_a_Light_Bulb/project.js
@@ -1,5 +1,218 @@
 async function preload1() {
     await displayBasicInformation();
+    let simulationControl = createDiv();
+    simulationControl.parent("ledBoardSimulation");
+    simulationControl.id("simulationControl");
+
+    let colorControl = createDiv();
+    colorControl.parent("simulationControl");
+    colorControl.id("colorControl");
+    colorControl.style("display", "flex");
+    colorControl.style("align-items", "center");
+
+    let colorControlTitle = createElement("p", "change the color of the light:");
+    colorControlTitle.style("margin-right", "1.5rem");
+    colorControlTitle.parent("colorControl");
+
+    colorPicker = createInput();
+    colorPicker.attribute("type", "color");
+    colorPicker.parent("colorControl");
+    colorPicker.style("width", "50px");
+    colorPicker.style("height", "50px");
+    colorPicker.input(processColorInput);
+
+    let warmthControl = createDiv();
+    warmthControl.parent("simulationControl");
+    warmthControl.id("warmthControl");
+    warmthControl.style("display", "flex");
+    warmthControl.style("flex-direction", "column");
+
+    let warmthControlLabel = createElement("p", "change the warmth of the light(warm to cool):");
+    warmthControlLabel.style("margin-right", "1.5rem");
+    warmthControlLabel.parent("warmthControl");
+    warmthControlSlider = createSlider(0, 100, 50, 1);
+    warmthControlSlider.parent("warmthControl");
+    warmthControlSlider.style("width", "30rem");
+
+    let brightnessControl = createDiv();
+    brightnessControl.parent("simulationControl");
+    brightnessControl.id("brightnessControl");
+    brightnessControl.style("display", "flex");
+    brightnessControl.style("flex-direction", "column");
+
+    let brightnessControlLabel = createElement("p", "change the brightness of the light(low to high):");
+    brightnessControlLabel.parent("brightnessControl");
+    brightnessControlSlider = createSlider(0, 100, 50, 1);
+    brightnessControlSlider.parent("brightnessControl");
+    brightnessControlSlider.style("width", "30rem");
+
+    brightnessControlSlider.input(() => processColorInput("brightnessControlSlider"));
+    warmthControlSlider.input(() => processColorInput("warmthControlSlider"));
+    colorPicker.input(() => processColorInput("colorPicker"));
+
+    let colorChangeSpeedControl = createDiv();
+    colorChangeSpeedControl.parent("simulationControl");
+    colorChangeSpeedControl.id("colorChangeSpeedControl");
+    colorChangeSpeedControl.style("display", "flex");
+    colorChangeSpeedControl.style("flex-direction", "column");
+
+    let colorChangeSpeedControlTitle = createElement("p", "change the speed of the color change(low to high):");
+    colorChangeSpeedControlTitle.parent("colorChangeSpeedControl");
+
+    let colorChangeSpeedSlider = createSlider(0.1, 0.7, 0.7, 0.01);
+    colorChangeSpeedSlider.parent("colorChangeSpeedControl");
+    colorChangeSpeedSlider.style("width", "30rem");
+    colorChangeSpeedSlider.input(() => {
+        colorChangeSpeed = colorChangeSpeedSlider.value();
+    }
+    );
+
+
+    let patternControl = createDiv();
+    patternControl.parent("simulationControl");
+    patternControl.id("patternControl");
+    patternControl.style("display", "flex");
+
+    patternControl.style("flex-direction", "column");
+
+    let patternControlTitle = createElement("p", "choose the pattern:");
+    patternControlTitle.style("margin-right", "1.5rem");
+    patternControlTitle.parent("patternControl");
+
+    let patternPicker = createDiv();
+    patternPicker.id("patternPicker");
+    patternPicker.parent("patternControl");
+    patternPicker.style("display", "flex");
+    patternPicker.style("gap", "1.5rem");
+
+
+    let patternDiag = createDiv();
+    patternDiag.id("patternDiag");
+    patternDiag.parent("patternPicker");
+    patternDiag.style("display", "flex");
+    patternDiag.style("align-items", "center");
+    patternDiag.style("justify-content", "center");
+
+
+    patternDiagPicker = createCheckbox("", false);
+    patternDiagPicker.id("patternDiagPicker");
+    patternDiagPicker.style("transform-origin", "center");
+    patternDiagPicker.style("transform", "scale(3)");
+    patternDiagPicker.changed(() => {
+        if (patternDiagPicker.checked()) {
+            pattern.push("diag");
+        } else {
+            pattern = pattern.filter(p => p !== "diag");
+        }
+    });
+
+
+    patternDiagPicker.style("margin-right", "1.5rem");
+
+    patternDiagPicker.parent("patternDiag");
+
+    let patternDiagPickerLabel = createElement("label", "diag");
+    patternDiagPickerLabel.parent("patternDiag");
+    patternDiagPickerLabel.style("margin-right", "1.5rem");
+    patternDiagPickerLabel.style("fontSize", "2rem");
+
+
+    let patternCross = createDiv();
+    patternCross.id("patternCross");
+    patternCross.parent("patternPicker");
+    patternCross.style("display", "flex");
+    patternCross.style("align-items", "center");
+    patternCross.style("justify-content", "center");
+
+
+
+    patternCrossPicker = createCheckbox("", true);
+    patternCrossPicker.id("patternCrossPicker");
+    patternCrossPicker.style("transform-origin", "center");
+    patternCrossPicker.style("transform", "scale(3)");
+    patternCrossPicker.changed(() => {
+        if (patternCrossPicker.checked()) {
+            pattern.push("cross");
+        } else {
+            pattern = pattern.filter(p => p !== "cross");
+        }
+    });
+
+
+    patternCrossPicker.style("margin-right", "1.5rem");
+    patternCrossPicker.parent("patternCross");
+
+    let patternCrossPickerLabel = createElement("label", "cross");
+    patternCrossPickerLabel.parent("patternCross");
+    patternCrossPickerLabel.style("margin-right", "1.5rem");
+    patternCrossPickerLabel.style("fontSize", "2rem");
+
+    let patternSurround = createDiv();
+    patternSurround.id("patternSurround");
+    patternSurround.parent("patternPicker");
+    patternSurround.style("display", "flex");
+    patternSurround.style("align-items", "center");
+    patternSurround.style("justify-content", "center");
+
+
+    patternSurroundPicker = createCheckbox("", false);
+    patternSurroundPicker.id("patternSurroundPicker");
+    patternSurroundPicker.style("transform-origin", "center");
+    patternSurroundPicker.style("transform", "scale(3)");
+    patternSurroundPicker.changed(() => {
+        if (patternSurroundPicker.checked()) {
+            pattern.push("surround");
+        } else {
+            pattern = pattern.filter(p => p !== "surround");
+        }
+    });
+
+
+    patternSurroundPicker.style("margin-right", "1.5rem");
+    patternSurroundPicker.parent("patternSurround");
+
+    let patternSurroundPickerLabel = createElement("label", "surround");
+    patternSurroundPickerLabel.parent("patternSurround");
+    patternSurroundPickerLabel.style("margin-right", "1.5rem");
+    patternSurroundPickerLabel.style("fontSize", "2rem");
+
+    let randomTurnOnControl = createDiv();
+    randomTurnOnControl.parent("simulationControl");
+    randomTurnOnControl.id("randomTurnOnControl");
+    randomTurnOnControl.style("display", "flex");
+    randomTurnOnControl.style("flex-direction", "column");
+
+    let randomTurnOnControlTitle = createElement("p", "frequency of random turn on(random to stable):");
+    randomTurnOnControlTitle.style("margin-right", "1.5rem");
+    randomTurnOnControlTitle.parent("randomTurnOnControl");
+
+    let randomTurnOnPicker = createSlider(0.3, 1, 0.3, 0.01);
+    randomTurnOnPicker.parent("randomTurnOnControl");
+    randomTurnOnPicker.style("width", "30rem");
+    randomTurnOnPicker.input(() => {
+        randomness = randomTurnOnPicker.value();
+    });
+
+    let colorSpreadNoiseControl = createDiv();
+    colorSpreadNoiseControl.parent("simulationControl");
+    colorSpreadNoiseControl.id("colorSpreadNoiseControl");
+    colorSpreadNoiseControl.style("display", "flex");
+    colorSpreadNoiseControl.style("flex-direction", "column");
+
+    let colorSpreadNoiseControlTitle = createElement("p", "color spread noise(low to high):");
+    colorSpreadNoiseControlTitle.style("margin-right", "1.5rem");
+    colorSpreadNoiseControlTitle.parent("colorSpreadNoiseControl");
+
+    let colorSpreadNoiseSlider = createSlider(0, 20, 0, 1);
+    colorSpreadNoiseSlider.parent("colorSpreadNoiseControl");
+    colorSpreadNoiseSlider.style("width", "30rem");
+    colorSpreadNoiseSlider.input(() => {
+        noise = colorSpreadNoiseSlider.value();
+    });
+
+
+
+
 }
 
 class led {
@@ -352,219 +565,7 @@ async function setup1() {
     canvas.parent("ledBoardSimulation");
     colorMode(HSB, H_MAX, S_MAX, B_MAX, ALPHA_MAX);
 
-    let simulationControl = createDiv();
-    simulationControl.parent("ledBoardSimulation");
-    simulationControl.id("simulationControl");
-
-    let colorControl = createDiv();
-    colorControl.parent("simulationControl");
-    colorControl.id("colorControl");
-    colorControl.style("display", "flex");
-    colorControl.style("align-items", "center");
-
-    let colorControlTitle = createElement("p", "change the color of the light:");
-    colorControlTitle.style("margin-right", "1.5rem");
-    colorControlTitle.parent("colorControl");
-
-    colorPicker = createInput();
-    colorPicker.attribute("type", "color");
-    colorPicker.parent("colorControl");
-    colorPicker.style("width", "50px");
-    colorPicker.style("height", "50px");
-    colorPicker.input(processColorInput);
-
-    let warmthControl = createDiv();
-    warmthControl.parent("simulationControl");
-    warmthControl.id("warmthControl");
-    warmthControl.style("display", "flex");
-    warmthControl.style("flex-direction", "column");
-
-    let warmthControlLabel = createElement("p", "change the warmth of the light(warm to cool):");
-    warmthControlLabel.style("margin-right", "1.5rem");
-    warmthControlLabel.parent("warmthControl");
-    warmthControlSlider = createSlider(0, 100, 50, 1);
-    warmthControlSlider.parent("warmthControl");
-    warmthControlSlider.style("width", "30rem");
-
-    let brightnessControl = createDiv();
-    brightnessControl.parent("simulationControl");
-    brightnessControl.id("brightnessControl");
-    brightnessControl.style("display", "flex");
-    brightnessControl.style("flex-direction", "column");
-
-    let brightnessControlLabel = createElement("p", "change the brightness of the light(low to high):");
-    brightnessControlLabel.parent("brightnessControl");
-    brightnessControlSlider = createSlider(0, 100, 50, 1);
-    brightnessControlSlider.parent("brightnessControl");
-    brightnessControlSlider.style("width", "30rem");
-
-    brightnessControlSlider.input(() => processColorInput("brightnessControlSlider"));
-    warmthControlSlider.input(() => processColorInput("warmthControlSlider"));
-    colorPicker.input(() => processColorInput("colorPicker"));
-
-    let colorChangeSpeedControl = createDiv();
-    colorChangeSpeedControl.parent("simulationControl");
-    colorChangeSpeedControl.id("colorChangeSpeedControl");
-    colorChangeSpeedControl.style("display", "flex");
-    colorChangeSpeedControl.style("flex-direction", "column");
-
-    let colorChangeSpeedControlTitle = createElement("p", "change the speed of the color change(low to high):");
-    colorChangeSpeedControlTitle.parent("colorChangeSpeedControl");
-
-    let colorChangeSpeedSlider = createSlider(0.1, 0.7, 0.7, 0.01);
-    colorChangeSpeedSlider.parent("colorChangeSpeedControl");
-    colorChangeSpeedSlider.style("width", "30rem");
-    colorChangeSpeedSlider.input(() => {
-        colorChangeSpeed = colorChangeSpeedSlider.value();
-    }
-    );
-
-
-    let patternControl = createDiv();
-    patternControl.parent("simulationControl");
-    patternControl.id("patternControl");
-    patternControl.style("display", "flex");
-
-    patternControl.style("flex-direction", "column");
-
-    let patternControlTitle = createElement("p", "choose the pattern:");
-    patternControlTitle.style("margin-right", "1.5rem");
-    patternControlTitle.parent("patternControl");
-
-    let patternPicker = createDiv();
-    patternPicker.id("patternPicker");
-    patternPicker.parent("patternControl");
-    patternPicker.style("display", "flex");
-    patternPicker.style("gap", "1.5rem");
-
-
-    let patternDiag = createDiv();
-    patternDiag.id("patternDiag");
-    patternDiag.parent("patternPicker");
-    patternDiag.style("display", "flex");
-    patternDiag.style("align-items", "center");
-    patternDiag.style("justify-content", "center");
-
-
-    patternDiagPicker = createCheckbox("", false);
-    patternDiagPicker.id("patternDiagPicker");
-    patternDiagPicker.style("transform-origin", "center");
-    patternDiagPicker.style("transform", "scale(3)");
-    patternDiagPicker.changed(() => {
-        if (patternDiagPicker.checked()) {
-            pattern.push("diag");
-        } else {
-            pattern = pattern.filter(p => p !== "diag");
-        }
-    });
-
-
-    patternDiagPicker.style("margin-right", "1.5rem");
-
-    patternDiagPicker.parent("patternDiag");
-
-    let patternDiagPickerLabel = createElement("label", "diag");
-    patternDiagPickerLabel.parent("patternDiag");
-    patternDiagPickerLabel.style("margin-right", "1.5rem");
-    patternDiagPickerLabel.style("fontSize", "2rem");
-
-
-    let patternCross = createDiv();
-    patternCross.id("patternCross");
-    patternCross.parent("patternPicker");
-    patternCross.style("display", "flex");
-    patternCross.style("align-items", "center");
-    patternCross.style("justify-content", "center");
-
-
-
-    patternCrossPicker = createCheckbox("", true);
-    patternCrossPicker.id("patternCrossPicker");
-    patternCrossPicker.style("transform-origin", "center");
-    patternCrossPicker.style("transform", "scale(3)");
-    patternCrossPicker.changed(() => {
-        if (patternCrossPicker.checked()) {
-            pattern.push("cross");
-        } else {
-            pattern = pattern.filter(p => p !== "cross");
-        }
-    });
-
-
-    patternCrossPicker.style("margin-right", "1.5rem");
-    patternCrossPicker.parent("patternCross");
-
-    let patternCrossPickerLabel = createElement("label", "cross");
-    patternCrossPickerLabel.parent("patternCross");
-    patternCrossPickerLabel.style("margin-right", "1.5rem");
-    patternCrossPickerLabel.style("fontSize", "2rem");
-
-    let patternSurround = createDiv();
-    patternSurround.id("patternSurround");
-    patternSurround.parent("patternPicker");
-    patternSurround.style("display", "flex");
-    patternSurround.style("align-items", "center");
-    patternSurround.style("justify-content", "center");
-
-
-    patternSurroundPicker = createCheckbox("", false);
-    patternSurroundPicker.id("patternSurroundPicker");
-    patternSurroundPicker.style("transform-origin", "center");
-    patternSurroundPicker.style("transform", "scale(3)");
-    patternSurroundPicker.changed(() => {
-        if (patternSurroundPicker.checked()) {
-            pattern.push("surround");
-        } else {
-            pattern = pattern.filter(p => p !== "surround");
-        }
-    });
-
-
-    patternSurroundPicker.style("margin-right", "1.5rem");
-    patternSurroundPicker.parent("patternSurround");
-
-    let patternSurroundPickerLabel = createElement("label", "surround");
-    patternSurroundPickerLabel.parent("patternSurround");
-    patternSurroundPickerLabel.style("margin-right", "1.5rem");
-    patternSurroundPickerLabel.style("fontSize", "2rem");
-
-    let randomTurnOnControl = createDiv();
-    randomTurnOnControl.parent("simulationControl");
-    randomTurnOnControl.id("randomTurnOnControl");
-    randomTurnOnControl.style("display", "flex");
-    randomTurnOnControl.style("flex-direction", "column");
-
-    let randomTurnOnControlTitle = createElement("p", "frequency of random turn on(random to stable):");
-    randomTurnOnControlTitle.style("margin-right", "1.5rem");
-    randomTurnOnControlTitle.parent("randomTurnOnControl");
-
-    let randomTurnOnPicker = createSlider(0.3, 1, 0.3, 0.01);
-    randomTurnOnPicker.parent("randomTurnOnControl");
-    randomTurnOnPicker.style("width", "30rem");
-    randomTurnOnPicker.input(() => {
-        randomness = randomTurnOnPicker.value();
-    });
-
-    let colorSpreadNoiseControl = createDiv();
-    colorSpreadNoiseControl.parent("simulationControl");
-    colorSpreadNoiseControl.id("colorSpreadNoiseControl");
-    colorSpreadNoiseControl.style("display", "flex");
-    colorSpreadNoiseControl.style("flex-direction", "column");
-
-    let colorSpreadNoiseControlTitle = createElement("p", "color spread noise(low to high):");
-    colorSpreadNoiseControlTitle.style("margin-right", "1.5rem");
-    colorSpreadNoiseControlTitle.parent("colorSpreadNoiseControl");
-
-    let colorSpreadNoiseSlider = createSlider(0, 20, 0, 1);
-    colorSpreadNoiseSlider.parent("colorSpreadNoiseControl");
-    colorSpreadNoiseSlider.style("width", "30rem");
-    colorSpreadNoiseSlider.input(() => {
-        noise = colorSpreadNoiseSlider.value();
-    });
-
-
-
-
+    
 
     ellipseMode(CORNER);
     textAlign(CENTER, CENTER);
